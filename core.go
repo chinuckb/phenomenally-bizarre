452f1606ac29db27241f34fc4490346ad7292681
chinuckb <chinu1991999@gmail.com>
2024-10-25 23:03:14 +0200

    Made Few Changes

--- core.go
+++ core.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var tmpl = template.Must(template.ParseFiles("templates/dashboard.html"))
 
 func main() {
-	fmt.Println("We Good")
+	r := mux.NewRouter()
+	r.HandleFunc("/", dashboardHandler)
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.ListenAndServe(":8080", r)
+}
+
+// Dashboard handler
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
 }
--- go.mod
+++ go.mod
@@ -1,2 +1,8 @@
 module hello
+
 go 1.23.2
+
+require (
+	github.com/go-echarts/go-echarts/v2 v2.4.3 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+)
